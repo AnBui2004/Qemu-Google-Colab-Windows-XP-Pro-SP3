78ef9a8db434a17366b8c1d5f057c32445999685
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-26 16:47:38 +0700

    Create Run3.sh

---
+++ Run3.sh
@@ -0,0 +1,15 @@
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=XP.iso,media=cdrom \
+  -drive file=XP.img,format=raw \
+  -drive file=MyPal.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu coreduo \
+  -smp cores=2 \
+  -device rtl8139,netdev=n0 -netdev user,id=n0 \
+  -accel tcg,thread=multi \
