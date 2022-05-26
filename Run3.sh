65907906b5ab59f8298b9edc355749b16e7e3068
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-26 16:48:35 +0700

    Update Run3.sh

--- Run3.sh
+++ Run3.sh
@@ -3,13 +3,14 @@ sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=d \
+  -drive file=XP.img \
   -drive file=XP.iso,media=cdrom \
-  -drive file=XP.img,format=raw \
   -drive file=MyPal.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
   -cpu coreduo \
-  -smp cores=2 \
+  -smp sockets=1,cores=4,threads=1 \
+  -vga std \
   -device rtl8139,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
